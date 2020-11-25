117f58849fea61826a88179765089e19a509db90
thanhda <thanhda1993@gmail.com>
2020-11-25 21:29:20 +0700

    update schema db

--- src/models/ChiTieu.js
+++ src/models/ChiTieu.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-const thuChiSchema = mongoose.Schema({
+const thoigianSchema = mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
     year: {
         type: String,
         required: true,
@@ -10,51 +14,65 @@ const thuChiSchema = mongoose.Schema({
         type: String,
         required: true,
         trim: true
-    },
-    thunhap: {
-        type: Number,
-        required: true
-    },
-    chitieu: {
-        tietKiem: {
-            type: Number,
-            required: true
-        },
-        chiTieuBatBuoc: {
-            type: Number,
-            required: true
-        },
-        chiTieuHangNgayDuDinh: [{
-            day: {
-                type: String,
-                required: true
-            },
-            sotien: {
-                type: Number,
-                required: true
-            }
-        }],
-        chiTieuHangNgayThucTe: [{
-            day: {
-                type: String,
-                required: true
-            },
-            sotien: {
-                type: Number
-            }
-        }]
     }
 });
 
-thuChiSchema.statics.findChiTieuByDate = async function (date, month, year) {
-    const chiTieu = await this.findOne({
-        year,
-        month
-    });
-    return chiTieu;
-}
+const thunhapSchema = mongoose.Schema({
+    key: String,
+    sotien: Number,
+    required: true
+});
+
+const chitieuThangSchema = mongoose.Schema({
+    key: String,
+    tienTietKiem: Number,
+    chiTieuBatBuoc: Number
+});
+
+const chiTieuHangNgayDuDinhSchema = mongoose.Schema({
+    key: String,
+    ngay: {
+        type: String,
+        required: true
+    },
+    sotien: {
+        type: Number,
+        required: true
+    }
+});
+
+const chiTieuHangNgayThucTeSchema = mongoose.Schema({
+    key: String,
+    ngay: {
+        type: String,
+        required: true
+    },
+    sotien: {
+        type: Number,
+        required: true
+    },
+    tieude: {
+        type: String
+    },
+    noidung: {
+        type: String
+    }
+});
 
 
 
-const Chitieu = mongoose.model('Chitieu', thuChiSchema);
-module.exports = Chitieu;
+const Thoigian = mongoose.model('Thoigian', thoigianSchema);
+const Thunhap = mongoose.model('Thunhap', thunhapSchema);
+const ChitieuThang = mongoose.model('ChitieuThang', chitieuThangSchema);
+const ChiTieuHangNgayDuDinh = mongoose.model('ChiTieuHangNgayDuDinh', chiTieuHangNgayDuDinhSchema);
+const ChiTieuHangNgayThucte = mongoose.model('ChiTieuHangNgayThucte', chiTieuHangNgayThucTeSchema);
+
+const thuChi = {
+    Thoigian,
+    Thunhap,
+    ChitieuThang,
+    ChiTieuHangNgayDuDinh,
+    ChiTieuHangNgayThucte
+};
+
+module.exports = thuChi;
--- src/routes/chitieu.js
+++ src/routes/chitieu.js
@@ -1,38 +1,24 @@
 const express = require('express');
-const ChiTieu = require('../models/ChiTieu');
+const {
+    Thoigian,
+    Thunhap,
+    ChitieuThang,
+    ChiTieuHangNgayDuDinh,
+    ChiTieuHangNgayThucte
+} = require('../models/ChiTieu');
 const auth = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/chitieu', async (req, res) => {
-    console.log('/chitieu');
-    const chitieuModel = {
-        year: '2020',
-        month: '11',
-        thunhap: 1000000,
-        chitieu: {
-            tietKiem: 500000,
-            chiTieuBatBuoc: 300000,
-            chiTieuHangNgayDuDinh: [{
-                day: 1,
-                sotien: 10000
-            }],
-            chiTieuHangNgayThucTe: [{
-                day: 1,
-                sotien: 20000
-            }]
-        }
-    };
-    const chitieu = new ChiTieu(chitieuModel);
-    await chitieu.save();
-    return res.status(200).json(chitieu);
+    
 });
 
 router.get('/chitieu/:day/:month/:year', async (req, res) => {
     const {
         day, month, year
     } = req.params;
-    console.log('[/chitieu/:day/:month/:year]:'+ day)
+    console.log('[/chitieu/:day/:month/:year]:' + day)
     const chitieuInfo = await ChiTieu.findChiTieuByDate(day, month, year);
     return res.status(200).json(chitieuInfo);
 });
